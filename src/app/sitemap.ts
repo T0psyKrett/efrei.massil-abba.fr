8506fd501481e26cccf411e8103f807058dbd654
Massil <massilabba06@gmail.com>
2026-03-18 19:48:43 +0100

    fix: make sitemap dynamic to fix build error

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next';
 import { getProjects, getReports } from '@/services/firestoreService';
 
+export const dynamic = 'force-dynamic';
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const baseUrl = 'https://efrei.massil-abba.fr';
 
